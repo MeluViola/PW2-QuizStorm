abbdc80dc0de993027b2e5eae53e5cdce7531824
Melina <violamelina.b@gmail.com>
2024-10-17 11:16:06 -0300

    Agregado de base de datos

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+$host = 'localhost';
+$db = 'quizstorm';
+$user = 'root';
+$pass = 'Mysqlunlam';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "Conexión exitosa a la base de datos.";
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+//Verificacion temporal de la bdd
+
