3029879066bf72706652828558e25517db871a0d
Rocio <nemethrocio@gmail.com>
2024-10-19 16:59:05 -0300

    Correccion ruta del index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QuizStorm</title>
-    <link rel="stylesheet" href="/TP-Final-QuizStorm/views/css/pagInicio.css">
+    <link rel="stylesheet" href="/TP-Final-QuizStorm/public/pagInicio.css">
 </head>
 <body>
 <div class="container">
