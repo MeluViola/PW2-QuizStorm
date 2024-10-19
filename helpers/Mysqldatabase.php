fda2e40e08d2db6b02ab47cd8c50685dcec1d4ce
Melina <violamelina.b@gmail.com>
2024-10-19 11:51:45 -0300

    Configuracion de acceso a la bdd (config.ini)

---
+++ helpers/Mysqldatabase.php
@@ -0,0 +1,22 @@
+<?php
+
+class Mysqldatabase
+{
+    private $connection;
+    public function __construct($host, $username, $password, $database)
+    {
+        $this -> connection = mysqli_connect($host, $username, $password, $database);
+        if(!$this -> connection){
+            die("Connection failed: " . mysqli_connect_error());
+        }
+    }
+
+    public function query($sql){
+        $result = mysqli_query($this -> connection, $sql);
+        return mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+
+    public function __destruct(){
+        mysqli_close($this -> connection);
+    }
+}
--- views/perfilUsuarioView.php
+++ views/perfilUsuarioView.php
@@ -1,7 +1,8 @@
 <?php
-include_once ("helpers/Mysqldatabase.php");
+include_once(__DIR__ . "/../helpers/Mysqldatabase.php");
 
-$config = parse_ini_file("config.ini");
+$config = parse_ini_file(__DIR__ . "/../config.ini"); //se usa para acceder a los archivos entre carpetas
+
 $database = new Mysqldatabase(
     $config ['host'],
     $config ['username'],
@@ -9,12 +10,9 @@ $database = new Mysqldatabase(
     $config ['database']
 );
 
-
 //Verificacion temporal de la bdd
 $datos = $database->query("SELECT * FROM usuario");
 
 foreach ($datos as $dato) {
     echo $dato['nombre_completo'] . "<br> " . $dato['nombre_usuario']. "<br> ";
 }
-
-
